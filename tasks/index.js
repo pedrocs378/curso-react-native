50dbb5383a3a45cfe8d29be771c5a0ddf95be774
Pedro Cesar <pedrocs378@gmail.com>
2020-08-28 18:01:19 -0300

    update project tasks

--- tasks/index.js
+++ tasks/index.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import TaskList from './src/screens/TaskList';
 import {name as appName} from './app.json';
+import TaskList from './src/screens/TaskList';
 
 AppRegistry.registerComponent(appName, () => TaskList);
