9b761433ce0d5af642b38584d4a7fb783baf8667
Pedro Cesar <pedrocs378@gmail.com>
2020-09-16 22:20:03 -0300

    inserindo posts no firebase

--- clone-instagram/mobile/src/store/actions/actionPosts.js
+++ clone-instagram/mobile/src/store/actions/actionPosts.js
@@ -3,9 +3,22 @@ import axios from 'axios'
 
 export const addPost = post => {
     return dispatch => {
-        axios.post('/posts.json', { ...post })
-            .then(res => console.log(res.data))
+        axios({
+            url: 'uploadImage',
+            baseURL: 'http://localhost:5001/clone-instagram-ca684/us-central1/uploadImage',
+            method: 'post',
+            data: {
+                image: post.image.base64
+            }
+        })
+            .then(res => {
+                post.image = res.data.imageUrl
+                axios.post('/posts.json', { ...post })
+                    .then(res => console.log(res.data))
+                    .catch(err => console.log(err))
+            })
             .catch(err => console.log(err))
+        
     }
 
     // return {
